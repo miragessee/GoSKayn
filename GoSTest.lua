24b322358e486071eecd8446986eaf31878cbd73
miragessee <miragessee@hotmail.com>
2018-04-18 20:11:30 +0300

    v2
    v2

--- GoSTest.lua
+++ GoSTest.lua
@@ -2,5 +2,5 @@ if GetObjectName(GetMyHero()) ~= "MissFortune" then return end
 
 if not pcall( require, "Inspired" ) then PrintChat("You are missing Inspired.lua!") return end
 
-myVar1 = "Hello World!"
-print(myVar1)
+PrintChat("MissFortune loaded.")
+PrintChat("ßy miragessee")
