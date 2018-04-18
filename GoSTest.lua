0be52b934a1c7bcf513177fbd848921ca904dd63
miragessee <miragessee@hotmail.com>
2018-04-18 20:09:14 +0300

    v1
    v1

--- GoSTest.lua
+++ GoSTest.lua
@@ -1,4 +1,6 @@
-require("Inspired.lua")
+if GetObjectName(GetMyHero()) ~= "MissFortune" then return end
+
+if not pcall( require, "Inspired" ) then PrintChat("You are missing Inspired.lua!") return end
 
 myVar1 = "Hello World!"
 print(myVar1)
